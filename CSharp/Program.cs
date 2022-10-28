18962a32cd4cc4eadc8bdd87ab88425ea39bb136
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 10:44:14 +0200

    Datatype String And Some Of Its Methods

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,38 +6,15 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // Declaring mutiple variables at once
-            int num3, num4, num5;
+            string myName = "Admin";
 
-            // Declaring a variable
-            int num1;
+            string message = "My name is " + myName;
 
-            // Assigning a value a variable
-            num1 = 13;
+            string capsMessage = message.ToUpper();
 
-            // Declaring and initializing a variable in one go
-            int num2 = 23;
-            num2 = 100;
+            string lowerCaseMessage = message.ToLower();
 
-            // Using concatination
-            Console.WriteLine("num1: " + num1);
-            Console.WriteLine("num2: " + num2);
-            int sum = num1 + num2;
-            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
-
-            double d1 = 3.1425;
-            double d2 = 5.1;
-            double dDiv = d1 / d2;
-            Console.WriteLine("d1/d2 is " + dDiv);
-
-            float f1 = 3.1425f;
-            float f2 = 5.1f;
-            float fDiv = f1 / f2;
-            Console.WriteLine("f1/f2 is " + fDiv);
-
-            double dIDiv = d1 / num1;
-            Console.WriteLine("d1/num1 is " + dIDiv);
-
+            Console.WriteLine(lowerCaseMessage);
             Console.Read();
         }
     }
