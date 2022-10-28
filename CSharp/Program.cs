2c96cb4c6cbb4120731fc455bb716f50c399c7b1
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 13:40:28 +0200

    Constants

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,35 +5,20 @@ namespace CSharp
 {
     internal class Program
     {
+        // Constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        // Create a constant of type string with your birthday as its value
+        // Using Birthday Generator
+        const string birthday = "12.21.1996";
+        const string birthday2 = "09.15.1997";
+        const string birthday3 = "1997-03-17";
+
         static void Main(string[] args)
         {
-            byte myByte = 25;
-            Console.WriteLine(myByte);
-            sbyte mySbyte = -15;
-            Console.WriteLine(mySbyte);
-            int myInt = -12318925;
-            Console.WriteLine(myInt);
-            uint myUint = 123091;
-            Console.WriteLine(myUint);
-            short myShort = -32768;
-            Console.WriteLine(myShort);
-            ushort myUShort = 65535;
-            Console.WriteLine(myUShort);
-            float myFloat = -31.1289f;
-            Console.WriteLine(myFloat);
-            double myDouble = -12.1231250;
-            Console.WriteLine(myDouble);
-            char myCharacter = 'A';
-            Console.WriteLine(myCharacter);
-            bool myBool = true;
-            Console.WriteLine(myBool);
-            string myText = "I control text";
-            Console.WriteLine(myText);
-            string numText = "15";
-
-            int myNumFromText = int.Parse(numText);
-            Console.WriteLine(myNumFromText);
-            Console.ReadLine();
+            Console.WriteLine("My birthday is always going to be: {0}", birthday);
+            Console.ReadKey();
         }
     }
 }
