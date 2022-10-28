3016debb3ade5f127d35a8651f51e665a6144c3d
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 13:31:26 +0200

    Solution For Exercise: Strings 2

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,21 +7,31 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            string myName;
+            Console.Write("Enter the string: ");
+            // Read first input string.
+            string input = Console.ReadLine();
 
-            Console.Write("Please enter your name and press enter: ");
+            Console.WriteLine("Enter a Character to search: ");
+            // Read the character input search.
+            char searchInput = Console.ReadLine()[0];
 
-            myName = Console.ReadLine();
+            // Gets the Index of the character from the string.
+            int searchIndex = input.IndexOf(searchInput);
+            // Prints the Index as a search result on console.
+            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
 
-            string myNameUpperCase = String.Format("Uppercase value: {0}", myName.ToUpper());
-            string myNameLowerCase = String.Format("Lowercase value: {0}", myName.ToLower());
-            string myNameTrimmed = String.Format("Trimmed value: {0}", myName.Trim());
-            string myNameSubString = String.Format("Substring value: {0}", myName.Substring(0, 5));
+            Console.Write("Enter first name: ");
+            // Read the first name.
+            string firstName = Console.ReadLine();
 
-            Console.WriteLine(myNameUpperCase);
-            Console.WriteLine(myNameLowerCase);
-            Console.WriteLine(myNameTrimmed);
-            Console.WriteLine(myNameSubString);
+            Console.Write("Enter last name: ");
+            // Read the last name.
+            string lastName = Console.ReadLine();
+
+            // Concatinate the firstName and lastName variable and assign that to fullName variable.
+            string fullNmae = string.Concat(firstName, " ", lastName);
+            // Prints the full name on the console.
+            Console.WriteLine("Your full name is {0}", fullNmae);
 
             Console.ReadKey();
         }
