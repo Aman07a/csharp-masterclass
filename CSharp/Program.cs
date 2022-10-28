6a02ee3b8c178246f3f29bc064054aea79842d4e
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 11:23:12 +0200

    Parsing a String To An Integer

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,27 +7,14 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            // Implicit conversion
-            int num = 12390532;
-            long bigNum = num;
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int resultInt = num1 + num2;
+            string result = myString + mySecondString;
 
-            float myFloat = 13.37F;
-            double myNewDouble = myFloat;
-
-            double myDouble = 13.37;
-            int myInt;
-            // Explicit conversion
-            // Cast double to int;
-            myInt = (int)myDouble;
-
-            // typeConversion
-            string myString = myDouble.ToString(); // "13.37"
-            string myFloatString = myFloat.ToString();
-            bool sunIsShining = false;
-
-            string myBoolString = sunIsShining.ToString();
-
-            Console.WriteLine(myBoolString);
+            Console.WriteLine(resultInt);
             Console.Read();
         }
     }
