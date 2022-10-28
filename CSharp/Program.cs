36fabbcb99ab0286252cdaa47e6d64a732d81629
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 10:39:51 +0200

    Datatypes Int, Float and Double

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,7 +6,39 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaring mutiple variables at once
+            int num3, num4, num5;
+
+            // Declaring a variable
+            int num1;
+
+            // Assigning a value a variable
+            num1 = 13;
+
+            // Declaring and initializing a variable in one go
+            int num2 = 23;
+            num2 = 100;
+
+            // Using concatination
+            Console.WriteLine("num1: " + num1);
+            Console.WriteLine("num2: " + num2);
+            int sum = num1 + num2;
+            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
+
+            double d1 = 3.1425;
+            double d2 = 5.1;
+            double dDiv = d1 / d2;
+            Console.WriteLine("d1/d2 is " + dDiv);
+
+            float f1 = 3.1425f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+            Console.WriteLine("f1/f2 is " + fDiv);
+
+            double dIDiv = d1 / num1;
+            Console.WriteLine("d1/num1 is " + dIDiv);
+
+            Console.Read();
         }
     }
 }
