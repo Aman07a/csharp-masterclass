f38db3325054882aa7e5f9b81b2e46346169c29a
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 10:55:26 +0200

    Starting point of our pogram

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,18 +4,17 @@ namespace CSharp
 {
     internal class Program
     {
+        // Starting point of our pogram
         static void Main(string[] args)
         {
-            string myName = "Admin";
-
-            string message = "My name is " + myName;
-
-            string capsMessage = message.ToUpper();
-
-            string lowerCaseMessage = message.ToLower();
-
-            Console.WriteLine(lowerCaseMessage);
+            Console.WriteLine("Hello Admin");
+            Console.WriteLine("C# is cool");
             Console.Read();
         }
+
+        /* This program
+         * is made, in order to 
+         * teach how to create a csharp example 
+        **/
     }
 }
