09f418bdc7912220f082968a937d7f663b0975ad
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-28 13:35:16 +0200

    Solution For The Challenge "Datatypes And Variables"

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,33 +7,33 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the string: ");
-            // Read first input string.
-            string input = Console.ReadLine();
+            byte myByte = 25;
+            Console.WriteLine(myByte);
+            sbyte mySbyte = -15;
+            Console.WriteLine(mySbyte);
+            int myInt = -12318925;
+            Console.WriteLine(myInt);
+            uint myUint = 123091;
+            Console.WriteLine(myUint);
+            short myShort = -32768;
+            Console.WriteLine(myShort);
+            ushort myUShort = 65535;
+            Console.WriteLine(myUShort);
+            float myFloat = -31.1289f;
+            Console.WriteLine(myFloat);
+            double myDouble = -12.1231250;
+            Console.WriteLine(myDouble);
+            char myCharacter = 'A';
+            Console.WriteLine(myCharacter);
+            bool myBool = true;
+            Console.WriteLine(myBool);
+            string myText = "I control text";
+            Console.WriteLine(myText);
+            string numText = "15";
 
-            Console.WriteLine("Enter a Character to search: ");
-            // Read the character input search.
-            char searchInput = Console.ReadLine()[0];
-
-            // Gets the Index of the character from the string.
-            int searchIndex = input.IndexOf(searchInput);
-            // Prints the Index as a search result on console.
-            Console.WriteLine("Index of character {0} in string is {1}", searchInput, searchIndex);
-
-            Console.Write("Enter first name: ");
-            // Read the first name.
-            string firstName = Console.ReadLine();
-
-            Console.Write("Enter last name: ");
-            // Read the last name.
-            string lastName = Console.ReadLine();
-
-            // Concatinate the firstName and lastName variable and assign that to fullName variable.
-            string fullNmae = string.Concat(firstName, " ", lastName);
-            // Prints the full name on the console.
-            Console.WriteLine("Your full name is {0}", fullNmae);
-
-            Console.ReadKey();
+            int myNumFromText = int.Parse(numText);
+            Console.WriteLine(myNumFromText);
+            Console.ReadLine();
         }
     }
 }
