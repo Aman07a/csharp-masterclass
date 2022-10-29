89f7ec008a684926b53e71efc2cbcba38079e879
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-29 17:47:29 +0200

    For Loops

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -9,41 +9,11 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            int inputTemperature = 0;
-            string temperatureMessage = string.Empty;
-            string inputValue = string.Empty;
-
-            // Takes input from console
-            Console.WriteLine("Enter the current temperature: ");
-            inputValue = Console.ReadLine();
-
-            // Validate the input as valid input integer value
-            bool validInteger = int.TryParse(inputValue, out inputTemperature);
-
-            if (validInteger)
+            for (int counter = 1; counter < 20; counter += 2)
             {
-                // If is valid integer then it will check for the conditions using nested ternary operator
-                temperatureMessage = inputTemperature <= 15 ?
-                // True
-                    "it is too cold here" :
-                // False
-                (inputTemperature >= 16 && inputTemperature <= 28) ?
-                // True
-                    "it is cold here" :
-                // False
-                inputTemperature > 28 ?
-                // True
-                    "it is hot here" :
-                // False
-                "";
-                Console.WriteLine(temperatureMessage);
+                Console.WriteLine(counter);
             }
-            else
-            {
-                // In case if the input value is not a valid temperature
-                Console.WriteLine("Not a valid Temperature");
-            }
-            Console.ReadKey();
+            Console.Read();
         }
     }
 }
