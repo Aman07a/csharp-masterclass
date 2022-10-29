a5e7bd4eebc5765dfb883746ddbe5ada2afccbe3
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-29 17:51:35 +0200

    Do While Loops

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -9,10 +9,17 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            for (int counter = 1; counter < 20; counter += 2)
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
             {
-                Console.WriteLine(counter);
-            }
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currentLength = nameOfAFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfAFriend;
+            } while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough!" + wholeText);
             Console.Read();
         }
     }
