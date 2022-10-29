7dcbb7c5770a03b625c8ab059624afae87bf5b2d
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-29 14:52:14 +0200

    Nested If Statements

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -5,24 +5,26 @@ namespace CSharp
 {
     internal class Program
     {
-        public static void Check(int number)
-        {
-            if (number % 2 == 0)
-            {
-                Console.WriteLine("Even: {0}", number);
-            }
-            else
-            {
-                Console.WriteLine("Odd: {0}", number);
-            }
-        }
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("Choose a number?");
-            string inputNumber = Console.ReadLine();
-            int.TryParse(inputNumber, out int number);
-            Check(number);
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+
+            Console.WriteLine("Please enter your username");
+            userName = Console.ReadLine();
+
+            if (isRegistered && userName != "" && userName.Equals("admin"))
+            {
+                Console.WriteLine("Hi there, registered user");
+                Console.WriteLine("Hi there, " + userName);
+                Console.WriteLine("Hi there, Admin");
+            }
+
+            if (isAdmin || isRegistered)
+            {
+                Console.WriteLine("You are logged in");
+            }
 
             Console.Read();
         }
