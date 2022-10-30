048a30e181873bd0da42024651b8cecd9f171e2f
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-30 08:50:50 +0100

    Using Multiple Constructors

--- CSharp/Human.cs
+++ CSharp/Human.cs
@@ -9,25 +9,69 @@ namespace CSharp
     class Human
     {
         // Member variables
-        private int age;
         private string firstName;
         private string lastName;
         private string eyeColor;
-        private bool isHuman = true;
+        private int age;
 
-        // Constructor
-        public Human(int age, string firstName, string lastName, string eyeColor)
+        // Default constructor
+        public Human()
         {
-            this.age = age;
+            Console.WriteLine("Constructor called. Object created");
+        }
+
+        public Human(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
             this.firstName = firstName;
             this.lastName = lastName;
             this.eyeColor = eyeColor;
         }
 
-        // Member method
-        public void IntroduceOnceself()
+        public Human(string firstName, string lastName, int age)
         {
-            Console.WriteLine("Hi, I'm {0} {1} {2}", firstName, lastName, eyeColor);
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        // Parameterized constructor
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        // Member method
+        public void IntroduceMyself()
+        {
+            if (age != 0 && lastName != null && eyeColor != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1} and {2} years old. My eye color is {3}", firstName, lastName, age, eyeColor);
+            else if (age != 0 && lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1} and {2} years old.", firstName, lastName, age);
+            }
+            else if (lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+            }
+            else if (firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} ", firstName);
+            }
+
         }
     }
 }
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -9,11 +9,25 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            Human denis = new Human(29, "Denis", "Panjuta", "green");
-            Human james = new Human(25, "James", "Bond", "blue");
-            denis.IntroduceOnceself();
-            james.IntroduceOnceself();
-            Console.Read();
+            Human denis = new Human("Denis", "Panjuta", "green", 29);
+            denis.IntroduceMyself();
+
+            Human amalia = new Human("Amalia", "Panjuta", "blue");
+            amalia.IntroduceMyself();
+
+            Human basicHuman = new Human();
+            basicHuman.IntroduceMyself();
+
+            Human natalie = new Human("Natalie");
+            natalie.IntroduceMyself();
+
+            Human michael = new Human("Michael", "Miller");
+            michael.IntroduceMyself();
+
+            Human frank = new Human("Frank", "Walter", 25);
+            frank.IntroduceMyself();
+
+            Console.ReadKey();
         }
     }
 }
