db45bc9494e9d6fb09a01ee77d97f1528e5203f8
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-30 16:03:56 +0100

    Properties

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -11,55 +11,16 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            Run.RunExercise();
-        }
+            Box box = new Box(3, 4,5);
 
-        public class Phone
-        {
-            public string Company;
-            public string Model;
-            public string ReleaseDay;
+            Console.WriteLine("Old Box width is " + box.Width);
+            box.Width = 10;
+            Console.WriteLine("New Box width is " + box.Width);
+            Console.WriteLine("Box volume is " + box.Volume);
 
-            // Place for your constructors
-            public Phone()
-            {
-                Company = "unknown";
-                Model = "unknown";
-                ReleaseDay = "unknown";
-            }
+            box.DisplayInfo();
 
-            public Phone(string company, string model)
-            {
-                Company = company;
-                Model = model;
-                ReleaseDay = "unknown";
-            }
-
-            public Phone(string company, string model, string releaseDay)
-            {
-                Company = company;
-                Model = model;
-                ReleaseDay = releaseDay;
-            }
-
-            public void Introduce()
-            {
-                Console.WriteLine("It is {0} created by {1}. It was released {2}.", Model, Company, ReleaseDay);
-            }
-        }
-
-        public class Run
-        {
-            public static void RunExercise()
-            {
-                Phone one = new Phone();
-                Phone two = new Phone("Apple", "IPhone 12");
-                Phone three = new Phone("Apple", "IPhone 12", "September 24, 2021");
-
-                one.Introduce();
-                two.Introduce();
-                three.Introduce();
-            }
+            Console.Read();
         }
     }
 }
