1e24da0008ce459c9b63f5389783147bb37233c5
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-30 19:26:14 +0100

    Foreach Loops and Switch Statement challenge

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -11,41 +11,77 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            Run();
+            Boolean valid = false;
+            string inputValueType;
+
+            Console.WriteLine("Enter a value: ");
+            string inputValue = Console.ReadLine();
+
+            Console.WriteLine("Select the Data type to validate the input you have entered");
+            Console.WriteLine("Press 1 for String");
+            Console.WriteLine("Press 2 for Integer");
+            Console.WriteLine("Press 3 for Boolean");
+
+            Console.WriteLine("Enter: ");
+            int inputType = Convert.ToInt32(Console.ReadLine());
+
+            switch (inputType)
+            {
+                case 1:
+                    // Check for string
+                    valid = IsAllAlphabetic(inputValue);
+                    inputValueType = "String";
+                    break;
+                case 2:
+                    int redValue = 0;
+                    // Check for integer
+                    valid = int.TryParse(inputValue, out redValue);
+                    inputValueType = "Integer";
+                    break;
+                case 3:
+                    bool redFlag = false;
+                    // Check for boolean
+                    valid = bool.TryParse(inputValue, out redFlag);
+                    inputValueType = "Boolean";
+                    break;
+                default:
+                    inputValueType = "Unknown";
+                    Console.WriteLine("Not able to detect the input type, something is wrong");
+                    break;
+            }
+
+            Console.WriteLine("You have entered a value: {0}", inputValue);
+
+            if(valid)
+            {
+                Console.WriteLine("It is valid: {0}", inputValueType);
+            } else
+            {
+                Console.WriteLine("It is invalid: {0}", inputValueType);
+            }
+
+            Console.ReadKey();
         }
 
-        public static void GetOdd(int[] Array)
+        /// <summary>
+        /// Function to check if the input string is valid string.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        ///
+        // H3LLO W0RLD
+
+        static bool IsAllAlphabetic(string value)
         {
-            // TODO
-            for (int i = 0; i < Array.Length; i++)
+            foreach (char c in value)
             {
-                if (Array[i] % 2 !=0)
+                if (!char.IsLetter(c))
                 {
-                    Console.WriteLine(Array[i]);
+                    return false;
                 }
             }
-        }
-
-        public static void GetEven(int[] Array)
-        {
-            // TODO
-            foreach (int value in Array)
-            {
-                if (value % 2 == 0)
-                {
-                    Console.WriteLine(value);
-                }
-            }
-        }
-
-        public static void Run()
-        {
-            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            GetOdd(array);
-            Console.WriteLine("---------");
-            GetEven(array);
-
+            return true;
         }
     }
+
 }
