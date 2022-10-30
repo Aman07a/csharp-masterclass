740dd9ef708ec48c9c9ce5684f8eb8cf02e3c00f
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-30 08:16:29 +0100

    Our First Own Class

---
+++ CSharp/Human.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    // This class is a blueprint for a datatype 
+
+    // Create a public variable lastName of type string.
+    // Change the IntroduceMyself method, so it tells the whole name.
+    // Create two objects with full information about themself - firstName and lastName
+
+    internal class Human
+    {
+        // Member variable
+        public string firstName;
+        public string lastName;
+
+        // Constructor
+        public Human(string myFirstName, string lastName)
+        {
+            firstName = myFirstName;
+            this.lastName = lastName;
+
+        }
+
+        // Member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
