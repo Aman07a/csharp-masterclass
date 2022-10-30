dcb42b8818498d250dacb07123962af97f8e4c51
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-30 16:50:36 +0100

    Members And Finalizers/Destructors

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -11,14 +11,9 @@ namespace CSharp
     {
         public static void Main(string[] args)
         {
-            Box box = new Box(5, 4, 10);
-
-            Console.WriteLine("Volume is {0}", box.Volume);
-            Console.WriteLine("Front surface of the box is {0}", box.FrontSurface);
-
-            box.DisplayInfo();
-
-            Console.Read();
+            Members member = new Members();
+            member.Introducing(true);
+            Console.ReadKey();
         }
     }
 }
